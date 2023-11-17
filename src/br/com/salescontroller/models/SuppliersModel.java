af3c6b3216006ee9eae8aa042819973d85ba1eb9
Juana Paiva <juana@ibm.com>
2023-11-17 08:11:01 -0300

    refactor: supplier model extends client model

--- src/br/com/salescontroller/models/SuppliersModel.java
+++ src/br/com/salescontroller/models/SuppliersModel.java
@@ -1,36 +1,8 @@
 package br.com.salescontroller.models;
 
-public class SuppliersModel {
-    
-    private Integer id;
-    private String name;
+public class SuppliersModel extends ClientsModel {
+
     private String cnpj;
-    private String email;
-    private String phone;
-    private String cellphone;
-    private String cep;
-    private String address;
-    private Integer addressNumber;
-    private String complement;
-    private String neighborhood;
-    private String city;
-    private String state;
-
-    public Integer getId() {
-        return this.id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getCnpj() {
         return this.cnpj;
@@ -38,85 +10,5 @@ public class SuppliersModel {
 
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCellphone() {
-        return this.cellphone;
-    }
-
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
-    }
-
-    public String getCep() {
-        return this.cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Integer getAddressNumber() {
-        return this.addressNumber;
-    }
-
-    public void setAddressNumber(Integer addressNumber) {
-        this.addressNumber = addressNumber;
-    }
-
-    public String getComplement() {
-        return this.complement;
-    }
-
-    public void setComplement(String complement) {
-        this.complement = complement;
-    }
-
-    public String getNeighborhood() {
-        return this.neighborhood;
-    }
-
-    public void setNeighborhood(String neighborhood) {
-        this.neighborhood = neighborhood;
-    }
-
-    public String getCity() {
-        return this.city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return this.state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 }
