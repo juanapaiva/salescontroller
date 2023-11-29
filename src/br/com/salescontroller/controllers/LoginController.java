844c8f8a1e76ed3bed3429021fde3f92f0dd91f9
Juana Paiva <juana@ibm.com>
2023-11-29 09:15:19 -0300

    refactor: email txt focus and login when enter pressed

--- src/br/com/salescontroller/controllers/LoginController.java
+++ src/br/com/salescontroller/controllers/LoginController.java
@@ -1,20 +1,28 @@
 package br.com.salescontroller.controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import br.com.salescontroller.dao.EmployeesDAO;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     private Stage stage;
     private Scene scene;
@@ -27,6 +35,24 @@ public class LoginController {
 
     @FXML
     private Button btnLogin;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Platform.runLater(() -> tfEmail.requestFocus());
+
+        btnLogin.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(keyEvent.getCode().equals(KeyCode.ENTER))
+                    try {
+                        btnLoginAction(new ActionEvent());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+            }
+            
+        });
+    }
 
     @FXML
     void btnLoginAction(ActionEvent event) throws IOException {
