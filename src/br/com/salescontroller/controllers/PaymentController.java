d6382d642fd51bd28b9fa119555ab6f21aafaee8
Juana Paiva <juana@ibm.com>
2023-11-29 06:50:56 -0300

    feat: payment page fxml

---
+++ src/br/com/salescontroller/controllers/PaymentController.java
@@ -0,0 +1,52 @@
+package br.com.salescontroller.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class PaymentController {
+
+    @FXML
+    private Button btnMenu;
+
+    @FXML
+    private Button btnPay;
+
+    @FXML
+    private TextField tfCard;
+
+    @FXML
+    private TextField tfChange;
+
+    @FXML
+    private TextField tfCheck;
+
+    @FXML
+    private TextField tfMoney;
+
+    @FXML
+    private TextField tfTotal;
+
+    @FXML
+    void btnMenuAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPayAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cepKeyPressedAction(KeyEvent event) {
+
+    }
+
+    @FXML
+    void checkNumberFormat(KeyEvent event) {
+
+    }
+
+}
